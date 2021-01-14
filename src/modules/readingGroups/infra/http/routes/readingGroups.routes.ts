51b2f466bfeef7fd3e54791bdec15a84f3165552
Matheus Adorno <matheusadorno11@gmail.com>
2021-01-14 17:03:14 -0300

    Middleware de rotas privadas

--- src/modules/readingGroups/infra/http/routes/readingGroups.routes.ts
+++ src/modules/readingGroups/infra/http/routes/readingGroups.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 
+import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import ReadingGroupsController from '../controllers/ReadingGroupsController';
 
 const readingGroupsRouter = Router();
 const readingGroupsController = new ReadingGroupsController();
+
+readingGroupsRouter.use(ensureAuthenticated);
 
 // readingGroupsRouter.get('/', async (request, response) => {
 //   const readingGroups = await readingGroupsRepository.find();
